607cc59d77c92a39d44cabd58709e7212171cdf3
SystemsBiologist <36110882+SystemsBiologist@users.noreply.github.com>
2019-12-19 22:09:37 +0100

    s4 class

--- CellMigRation/R/imports.R
+++ CellMigRation/R/imports.R
@@ -2,6 +2,6 @@
 #' @importFrom grDevices dev.off jpeg rainbow rgb chull
 #' @importFrom graphics lines points axis polygon title abline legend par
 #' @importFrom utils write.csv
-#' @import FME methods Hmisc sp vioplot rgl matrixStats SpatialTools stats
+#' @import FME methods Hmisc sp vioplot rgl matrixStats SpatialTools stats FactoMineR
 #'
 NULL
