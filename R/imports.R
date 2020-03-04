848464f83b119e4debdebfddbe57ae077f8a08a8
SystemsBiologist <36110882+SystemsBiologist@users.noreply.github.com>
2020-03-04 15:29:19 +0100

    Adding importfrom foreach %dopar%

--- R/imports.R
+++ R/imports.R
@@ -2,6 +2,7 @@
 #' @importFrom grDevices dev.off jpeg rainbow rgb chull
 #' @importFrom graphics lines points axis polygon title abline legend par
 #' @importFrom utils write.csv
+#' @importFrom foreach %dopar%
 #' @import FME methods Hmisc sp vioplot rgl matrixStats SpatialTools stats FactoMineR
 #'
 NULL
