4c074a3ea53f4f5590500afa267e80d4df225971
Waldir Leoncio <waldir.leoncio@gmail.com>
2020-03-25 08:52:12 +0100

    Added file importing and basic textual output

--- Shinyapp/app.R
+++ Shinyapp/app.R
@@ -1,4 +1,5 @@
 library(shiny)
+library(DT)
 
 # ==============================================================================
 # User interface
@@ -9,19 +10,42 @@ ui <- pageWithSidebar(
     # --------------------------------------------------------------------------
     # Sidebar panel for inputs
     # --------------------------------------------------------------------------
-    sidebarPanel(),
+    sidebarPanel(
+        fileInput("import", "Import trackedCells file")
+    ),
 
     # --------------------------------------------------------------------------
     # Main panel for displaying outputs
     # --------------------------------------------------------------------------
-    mainPanel()
+    mainPanel(
+        tabsetPanel(type="tabs",
+            tabPanel(
+                "Data preview",
+                
+                DT::dataTableOutput("data")
+            )
+        )
+    )
 )
 
 # ==============================================================================
 # Server logic
 # ==============================================================================
 server <- function(input, output) {
-
+    # --------------------------------------------------------------------------
+    # Show imported data
+    # --------------------------------------------------------------------------
+    output$data <- DT::renderDataTable({
+        req(input$import)
+        df <- get(load(input$import$datapath))
+        if (!is(df, "trackedCells")) {
+            stop(
+                "Wrong data format.",
+                " Please load a file containing an object",
+                " of class 'trackedCells'.")
+        }
+        return(df@images$images[[1]])
+    })
 }
 
 shinyApp(ui, server)
