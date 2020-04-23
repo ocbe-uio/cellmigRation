cc36020ee09cf05c371649b9e800abab3babf2c1
Waldir Leoncio <waldir.leoncio@gmail.com>
2020-04-23 13:16:15 +0200

    Readded rda datasets to package

---
+++ R/all_datasets.R
@@ -0,0 +1,27 @@
+#' Trajectories of 147 cells
+#'
+#' A dataset containing the coordinates and the ID of 147 cells from wound scratch migration experiment
+#'
+#' @docType data
+#' @keywords datasets
+#' @name WSAdataset
+#' @usage data(WSAdataset)
+#' @format A data frame with 11970 rows and 4 columns
+#' @examples
+#' data(WSAdataset)
+#'
+NULL
+
+#' Trajectories of 350 cells
+#'
+#' A dataset containing the coordinates and the ID of 350 cells from a dense random migration experiment
+#'
+#' @docType data
+#' @keywords datasets
+#' @name TrajectoryDataset
+#' @usage data(TrajectoryDataset)
+#' @format A data frame with 50216 rows and 4 columns
+#' @examples
+#' data(TrajectoryDataset)
+#'
+NULL
