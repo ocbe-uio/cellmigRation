5e5882795ec8dbbf401afe1a111468db89e7d670
Waldir Leoncio <w.l.netto@medisin.uio.no>
2020-07-09 11:39:56 +0200

    Improved output of rmPreProcessing (solves #22)

--- R/all_functions.R
+++ R/all_functions.R
@@ -3715,7 +3715,7 @@ rmPreProcessing = function(object, PixelSize=1.24,
   df<-object@adjDS
   df<-df[,1:3]                                        # Removing the unnecessary columns
   spl<-split(df,df[,1])
-  cat("This dataset contains: ",length(spl),"Cells","\n")
+  cat("This dataset contains:", length(spl), "cell(s) in total\n")
 
   tbd<-c()
   LENspl<-length(spl)
@@ -3732,7 +3732,7 @@ rmPreProcessing = function(object, PixelSize=1.24,
   df[,27]<-rep(NA,L)                                  # to be used for migration type
   colnames(df)<-c("ID","x","y","X","Y","dx","dy","dis","abs.ang","rel.ang.P","Cos.P","Persist.Time","Square Speed","cumDis","Dir.R","NewDX","NewDY","New.Abs.ang","Ang.Diff","New.Cos.diff","rel.ang.F","Cos.F","Forward.Persist.Time","MSD(lag)","VAC(lag)","Acceleration","M-type")
   ID_split <- split(df, df$ID)                        #Splitting the data frame based on the ID
-  cat("This dataset contains: ",length(ID_split),"Cells with more than three steps in their tracks","\n")
+  cat("This dataset contains:", length(ID_split), "cell(s) with more than three steps in their tracks\n")
 
   for(j in 1:length(ID_split)){                        # Having the ID =group order
     ID_split[[j]][1]=j
